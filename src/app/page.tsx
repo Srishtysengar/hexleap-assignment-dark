183126640e387510a32a9dcf42846667843e3587
Srishtysengar <srishtysengar120@gmail.com>
2024-03-28 10:53:46 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import SportsContainer from '../app/components/sportscontainer';
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+         <SportsContainer></SportsContainer>
+      </div>
+    </main>
+  );
+}
